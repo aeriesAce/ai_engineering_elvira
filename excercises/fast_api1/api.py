f990bc88e4935d99e3469b44360620ef9b1423f3
Elvira L <elvira.liljestrand@hotmail.com>
2025-09-16 16:16:44 +0200

    Excercise 4.1

--- excercises/fast_api1/api.py
+++ excercises/fast_api1/api.py
@@ -1,17 +1,32 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Query
 from .data_processing import Results
 
 app = FastAPI()
-router = APIRouter(prefix="/api/results")
+router = APIRouter(prefix="/api")
 
 @router.get("")
-async def read_results():
-    results = Results()
+async def read_results(limit: int = Query(100, gt=0)):
+    results = Results(limit)
     return results.json_response()
 
-@router.get("/schools")
+@router.get("/results/schools")
 async def filter_by_school(school: str):
     results = Results()
     return results.filter_school(school).json_response()
+
+@router.get("/results/filter")
+async def filter_by_fields(filter_:str):
+    results = Results()
+    return results.filter_fields(filter_).json_response()
+
+@router.get("/results/approved")
+async def appr():
+    results = Results()
+    return results.approved().json_response()
+
+@router.get("/results/denied")
+async def den():
+    results = Results()
+    return results.denied().json_response()
 
 app.include_router(router)
--- excercises/fast_api1/data_processing.py
+++ excercises/fast_api1/data_processing.py
@@ -2,22 +2,33 @@ import pandas as pd
 from .constants import DATA_PATH
 import json
 
-df = pd.read_excel(DATA_PATH / "resultat-ansokningsomgang-2024(2).xlsx", sheet_name = 4, skiprows = 5)
+df = pd.read_excel(DATA_PATH / "resultat-ansokningsomgang-2024(2).xlsx", sheet_name = "Tabell 3", header = 5)
 
 class Results:
     def __init__(self, limit=100):
-        self._df = df.head(limit)
         self._df_full = df
-
-    @property
-    def df(self):
-        return self._df
+        self._df = df.head(limit)
     
     def filter_school(self, school: str):
         mask = self._df_full["Utbildningsanordnare administrativ enhet"].str.casefold() == school.casefold()
         self._df = self._df_full[mask]
         return self
     
+    def filter_fields(self, filter_: str):
+        mask = self._df_full["Län"].str.casefold() == filter_.casefold()
+        self._df = self._df_full[mask]
+        return self
+    
+    def approved(self):
+        mask = self._df_full["Beslut"].astype(str).str.strip().str.casefold() == "beviljad"
+        self._df = self._df_full.loc[mask]
+        return self
+    
+    def denied(self):
+        mask = self._df_full["Beslut"].astype(str).str.strip().str.casefold() == "avslag"
+        self._df = self._df_full.loc[mask]
+        return self
+
     def json_response(self):
-        json_data = self.df.to_json(orient = "records")
+        json_data = self._df.to_json(orient = "records")
         return json.loads(json_data)
